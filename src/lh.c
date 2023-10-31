40a61705370f368f0608e232fe4b857890d312e6
João Paulo Lazzarini Cyrino <jpcyrino@gmail.com>
2023-10-31 11:10:29 -0300

    lh lexicon key now fixed string

--- src/lh.c
+++ src/lh.c
@@ -32,12 +32,18 @@ static mem_pool* create_mem_pool()
 
 void* mem_push(mem_pool* pool, size_t size)
 {
-    if(pool->pos > pool->size - 64)
+    if(size >= (pool->size - pool->pos))
+    {
+        pool->size += size;
+        pool->data = realloc(pool->data, pool->size);
+
+    }
+    if( pool->pos > pool->size - 64)
     {
         pool->size *= 2;
         pool->data = realloc(pool->data, pool->size);
     }
-    void* new_mem = pool->data + pool->pos;
+    void* new_mem = (void*) ((char*) pool->data + pool->pos);
     pool->pos += size;
     return new_mem;
 }
@@ -86,7 +92,9 @@ static void utf8_prev(char* str, size_t* i)
  
 #define HASH_MAGIC_NUMBER 5381
 #define DEFAULT_LEXICON_SIZE 1024
+#define DEFAULT_LEXICON_ITEM_SIZE 64
 #define MAX_OCCUPANCY_RATE 0.7
+
 
 static uint64_t 
 hash(const char* key)
@@ -104,7 +112,7 @@ typedef struct
 {
     bool occ;
     uint64_t count;
-    char* key;
+    char key[DEFAULT_LEXICON_ITEM_SIZE];
 } lexical_item;
 
 typedef struct
@@ -112,26 +120,33 @@ typedef struct
     uint64_t size;
     uint64_t occupancy;
     uint64_t total_counts;
-    lexical_item* items;  
-    mem_pool *string_storage;
+    lexical_item* items;
 } lexicon;
 
 static lexicon*
-lexicon_create(mem_pool* string_storage)
+lexicon_create()
 {
-    lexicon* lex = malloc(sizeof(*lex));
+    lexicon* lex = malloc(sizeof(lexicon));
     lex->occupancy = 0;
     lex->total_counts = 0;
     lex->size = DEFAULT_LEXICON_SIZE;
     lex->items = malloc(DEFAULT_LEXICON_SIZE * sizeof(lexical_item));
+      
     for(size_t i=0;i<DEFAULT_LEXICON_SIZE;i++)
     {
         lex->items[i].occ = false;
         lex->items[i].count = 0;
-        lex->items[i].key = NULL;
+        memset(lex->items[i].key,0,DEFAULT_LEXICON_ITEM_SIZE);
     }
-    lex->string_storage = string_storage;
+   
     return lex;
+}
+
+static void
+lexicon_release(lexicon* lex)
+{
+    free(lex->items);
+    free(lex);
 }
 
 static uint64_t
@@ -159,7 +174,7 @@ lexicon_get_cost(lexicon* lex, char* key)
 }
 
 static bool
-lexicon_add_item(lexicon* lex, char* key, uint64_t count, bool copy_key)
+lexicon_add_item(lexicon* lex, char* key, uint64_t count)
 {
 
     uint64_t hash_code = hash(key) % lex->size;
@@ -169,14 +184,7 @@ lexicon_add_item(lexicon* lex, char* key, uint64_t count, bool copy_key)
         // When slot not occupied: add item and return t
         if(!lex->items[hash_code].occ)
         {
-            if(copy_key)
-            {
-                lex->items[hash_code].key = 
-                    (char*) mem_push(lex->string_storage,strlen(key)+1);
-                strcpy(lex->items[hash_code].key,key);
-            }
-            else
-                lex->items[hash_code].key = key;
+            strcpy(lex->items[hash_code].key,key);
 
             lex->items[hash_code].count = count;
             lex->items[hash_code].occ = true;
@@ -206,7 +214,9 @@ lexicon_rehash(lexicon* lex)
 {
     size_t old_size = lex->size;
     lex->size *= 2;
+    
     lex->items = realloc(lex->items,lex->size * sizeof(lexical_item));
+
     for(size_t i=old_size;i<lex->size;i++)
         lex->items[i].occ = false;
     for(size_t i=0;i<lex->size;i++)
@@ -216,7 +226,7 @@ lexicon_rehash(lexicon* lex)
             lex->items[i].occ = false;
             lex->occupancy--;
             lex->total_counts -= lex->items[i].count;
-            if(!lexicon_add_item(lex,lex->items[i].key,lex->items[i].count,false))
+            if(!lexicon_add_item(lex,lex->items[i].key,lex->items[i].count))
                 return false;
         }
     }
@@ -228,7 +238,24 @@ lexicon_insert(lexicon* lex, char* key, uint64_t count)
 {
     float occupancy_rate = (float) lex->occupancy/lex->size;
     if(occupancy_rate >= MAX_OCCUPANCY_RATE && !lexicon_rehash(lex)) return false;
-    return lexicon_add_item(lex,key,count,true); 
+    return lexicon_add_item(lex,key,count); 
+}
+
+static char**
+lexicon_get_keys(lexicon* lex, mem_pool* storage, size_t* out_size)
+{
+    *out_size = lex->occupancy * sizeof(char*);
+    char** arr = (char**) mem_push(storage,*out_size);
+    size_t pos = 0;
+    for(size_t i=0;i<lex->size;i++)
+    {
+        if(lex->items[i].occ) 
+        {
+            arr[pos] = lex->items[i].key;
+            pos++;
+        }
+    }
+    return arr;
 }
 
 //
@@ -257,13 +284,15 @@ minseg_forward(lexicon* lex, mem_pool* str_storage, char*str,
     *cost = 0;
     *allocated_mem = 0;
 
+
     char* segment = NULL;
     size_t len = strlen(str);
     if(MINSEG_DEFAULT_BUFFER_SZ < len + 1) return NULL;
     double costs[MINSEG_DEFAULT_BUFFER_SZ + 1] = {0};
     char candidate_buff[MINSEG_DEFAULT_BUFFER_SZ];
     char min_cost_candidate[MINSEG_DEFAULT_BUFFER_SZ];
-    size_t i = 1, j = 0;
+
+    size_t i = 0, j = 0;
     while(i < len)
     {
         double min_cost = DBL_MAX;
@@ -280,7 +309,7 @@ minseg_forward(lexicon* lex, mem_pool* str_storage, char*str,
                 memset(min_cost_candidate,0,MINSEG_DEFAULT_BUFFER_SZ);
                 strcpy(min_cost_candidate,candidate_buff);
             }
-            //utf8_next(str,&j);
+
             j++;
         }
         costs[i+1] = min_cost;
@@ -291,7 +320,6 @@ minseg_forward(lexicon* lex, mem_pool* str_storage, char*str,
         strcpy(segment,min_cost_candidate);
         *n_segments += 1;
       
-        //utf8_next(str,&i);
         i++;
     }
     return segment;
@@ -304,7 +332,7 @@ minseg_backward(lexicon* lex, mem_pool* storage, char* segment, uint32_t* n_segm
 
     int64_t segment_pointers_index=0;
     int64_t n_segment = (int64_t) *n_segments;
-    while(n_segment>=0)
+    while(n_segment>0)
     {
         segment_pointers[segment_pointers_index] = segment;
         size_t len = strlen(segment);
@@ -323,7 +351,6 @@ minseg_backward(lexicon* lex, mem_pool* storage, char* segment, uint32_t* n_segm
 
     *allocated_mem = strlen(segment_pointers[segment_pointers_index]) + 1;
     char* init_of_segments = (char*) mem_push(storage, (*allocated_mem) * sizeof(char));
-    //assert(strcmp(init_of_segments, segment_pointers[0]) == 0);
 
     strcpy(init_of_segments,segment_pointers[segment_pointers_index]);
     segment_pointers_index--;
@@ -350,6 +377,71 @@ minseg(char* sentence, lexicon* lex, mem_pool* storage, uint32_t* out_n_segments
     return segs;
 }
 
+
+//
+// LEXHOUND
+//
+
+#define ALPHABET_DEFAULT_SIZE 64
+
+typedef struct
+{
+    uint64_t total_counts;
+    char letters[ALPHABET_DEFAULT_SIZE];
+    uint32_t counts[ALPHABET_DEFAULT_SIZE];
+} alphabet;
+
+
+static void
+alphabet_add(alphabet* ab, char ch)
+{
+    for(size_t i=0;i<ALPHABET_DEFAULT_SIZE;i++)
+    {
+        if(!ab->letters[i])
+        {
+            ab->letters[i] = ch;
+            ab->counts[i] = 1;
+            ab->total_counts += 1;
+            return;
+        }
+
+        if(ab->letters[i] == ch)
+        {
+            ab->counts[i] += 1;
+            ab->total_counts += 1;
+            return;
+        }
+    }
+
+}
+
+static alphabet*
+alphabet_create(mem_pool* storage, char* corpus, size_t corpus_sz)
+{
+    alphabet* ab = (alphabet*) mem_push(storage, sizeof(alphabet));
+    ab->total_counts = 0;
+    memset(ab->letters, 0,ALPHABET_DEFAULT_SIZE);
+    memset(ab->counts, 0, ALPHABET_DEFAULT_SIZE);
+    size_t pos = 0;
+    while(pos < corpus_sz)
+    {
+        char* wd = corpus + pos;
+        size_t len = strlen(wd);
+        
+        for(size_t i=0;i<len;i++)
+        {
+            alphabet_add(ab,wd[i]);
+        }
+        pos += 1 + len;
+    }
+    return ab;
+}
+
+static void
+lexhound_iteration_zero()
+{
+    
+}
 
 //
 // CORPUS LOAD
@@ -383,12 +475,16 @@ static void load_word_list(mem_pool* pool, char* filename, size_t* count)
 int main()
 { 
     mem_pool* corpus = create_mem_pool(); 
-    lexicon* le = lexicon_create(corpus); 
+    mem_pool* str_storage = create_mem_pool();
+    //lexicon* le = lexicon_create(str_storage); 
+    lexicon* le = lexicon_create();
     size_t count;
     load_word_list(corpus,"./test_res/wordlist.txt",&count);
     int i=0;
     size_t pos=0;
     char* str = (char*) corpus->data;
+    
+    alphabet* ab = alphabet_create(corpus, str, count);
     while(i < count)
     {
        lexicon_insert(le,str+pos,1);
@@ -396,18 +492,29 @@ int main()
        i++;
     }
 
+    printf("Total alfabeto: %zu\n", ab->total_counts);
+    size_t sz=0;
+    char** wds = lexicon_get_keys(le,corpus,&sz);
+    for(size_t i=0;i<le->occupancy;i++)
+    {
+        printf("%s %lf\n", wds[i],lexicon_get_cost(le,wds[i]));
+    }
+    mem_pop(corpus,sz);
+
+    printf("Itens lexicais únicos: %zu\n", le->occupancy);
+
      
 
     uint32_t n;
     size_t am;
     double cst;
 
-    printf("Teste minseg. Memória total alocada com corpus e léxico: %zu\n\n", corpus->size);
+    printf("Teste minseg. Memória total alocada com corpus e alfabeto: %zu\n\n", corpus->size);
     while(1)
     {
 
         char buff[144];
-        printf("Frase sem espaços: ");
+        printf("Frase sem espaços (Posição: %zu): ", corpus->pos);
         scanf("%s",buff);
 
         if(strcmp(buff,"q") ==0) break;
